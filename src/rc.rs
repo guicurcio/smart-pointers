20f7dccbe19cf9c0f34b065c149c519369cd0123
g <guidomaurocurcio@gmail.com>
2021-04-06 19:29:50 -0300

    Drop the pointer and use NonNull

--- src/rc.rs
+++ src/rc.rs
@@ -1,4 +1,5 @@
 use crate::cell::Cell;
+use std::ptr::NonNull;
 use std::usize;
 
 struct RcInner<T> {
@@ -7,7 +8,8 @@ struct RcInner<T> {
 }
 pub struct Rc<T> {
     // Cloning a box clones the T.
-    inner: *const RcInner<T>,
+    // Not safe to mutate this pointer -- this mutable pointer.
+    inner: NonNull<RcInner<T>>,
     // refcount?
     // refcount: usize,
     // It the value that it's shared among all the copies.
@@ -20,7 +22,7 @@ impl<T> Rc<T> {
             refcount: Cell::new(1),
         });
         Rc {
-            inner: Box::into_raw(inner),
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
         }
     }
 }
@@ -28,16 +30,35 @@ impl<T> Rc<T> {
 impl<T> std::ops::Deref for Rc<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
-        &unsafe { &*self.inner }.value
+        &unsafe { self.inner.as_ref() }.value
     }
 }
 
 impl<T> Clone for Rc<T> {
     // Increasing the reference count, but not copying the inner value. Only one of the inner value.
     fn clone(&self) -> Self {
-        let inner = unsafe { &*self.inner };
+        let inner = unsafe { self.inner.as_ref() };
         let c = inner.refcount.get();
         inner.refcount.set(c + 1);
         Rc { inner: self.inner }
+    }
+}
+
+impl<T> Drop for Rc<T> {
+    fn drop(&mut self) {
+        // Inner is a pointer to box +4
+        let inner = unsafe { self.inner.as_ref() };
+        let c = inner.refcount.get();
+        if c == 1 {
+            // Only reference.
+            // Inner must go away.
+            drop(inner);
+            unsafe {
+                // We drop the box and any pointer to that box is invalid.
+                let _ = Box::from_raw(self.inner.as_ptr());
+            }
+        } else {
+            inner.refcount.set(c - 1)
+        }
     }
 }
