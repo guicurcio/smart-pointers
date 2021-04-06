775b12a99cad3fe92b831a7b0e04f9e3f43e5306
g <guidomaurocurcio@gmail.com>
2021-04-06 18:11:21 -0300

    use cell to modify interior value

--- src/refcell.rs
+++ src/refcell.rs
@@ -1,5 +1,7 @@
+use crate::cell::Cell;
 use std::cell::UnsafeCell;
 
+#[derive(Clone, Copy)]
 enum RefState {
     Unshared,
     Shared(usize),
@@ -8,24 +10,24 @@ enum RefState {
 
 pub struct RefCell<T> {
     value: UnsafeCell<T>,
-    state: RefState,
+    state: crate::cell::Cell<RefState>,
 }
 
 impl<T> RefCell<T> {
     pub fn new(value: T) -> Self {
         Self {
             value: UnsafeCell::new(value),
-            state: RefState::Unshared,
+            state: Cell::new(RefState::Unshared),
         }
     }
     pub fn borrow(&self) -> Option<&T> {
-        match self.state {
+        match self.state.get() {
             RefState::Unshared => {
-                // self.state = RefState::Shared(1);
+                self.state.set(RefState::Shared(1));
                 Some(unsafe { &*self.value.get() })
             }
             RefState::Shared(n) => {
-                // self.state = RefState::Shared(n + 1);
+                self.state.set(RefState::Shared(n + 1));
                 Some(unsafe { &*self.value.get() })
             }
             RefState::Exclusive => None,
@@ -33,8 +35,8 @@ impl<T> RefCell<T> {
     }
 
     pub fn borrow_mut(&self) -> Option<&mut T> {
-        if let RefState::Unshared = self.state {
-            // self.state = RefState::Exclusive;
+        if let RefState::Unshared = self.state.get() {
+            self.state.set(RefState::Exclusive);
             Some(unsafe { &mut *self.value.get() })
         } else {
             None
