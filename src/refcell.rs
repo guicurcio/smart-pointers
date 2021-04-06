d6848b5070a1e6f6e5402ca9c699519c1094482f
g <guidomaurocurcio@gmail.com>
2021-04-06 18:04:31 -0300

    naively implement thread-unsafe code

--- src/refcell.rs
+++ src/refcell.rs
@@ -1,22 +1,43 @@
 use std::cell::UnsafeCell;
 
+enum RefState {
+    Unshared,
+    Shared(usize),
+    Exclusive,
+}
+
 pub struct RefCell<T> {
     value: UnsafeCell<T>,
-    reference: isize,
+    state: RefState,
 }
 
 impl<T> RefCell<T> {
     pub fn new(value: T) -> Self {
         Self {
             value: UnsafeCell::new(value),
-            reference: 0,
+            state: RefState::Unshared,
         }
     }
     pub fn borrow(&self) -> Option<&T> {
-        None
+        match self.state {
+            RefState::Unshared => {
+                // self.state = RefState::Shared(1);
+                Some(unsafe { &*self.value.get() })
+            }
+            RefState::Shared(n) => {
+                // self.state = RefState::Shared(n + 1);
+                Some(unsafe { &*self.value.get() })
+            }
+            RefState::Exclusive => None,
+        }
     }
 
     pub fn borrow_mut(&self) -> Option<&mut T> {
-        None
+        if let RefState::Unshared = self.state {
+            // self.state = RefState::Exclusive;
+            Some(unsafe { &mut *self.value.get() })
+        } else {
+            None
+        }
     }
 }
